129e1abf21ff8394e0f8c39f2ad50b5db11b1145
Le-Duude <57392459+Le-Duude@users.noreply.github.com>
2020-02-11 15:51:32 +0100

    header navigation plus effet d'hover sur liens début section candidats et recruteur

---
+++ inc/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700&display=swap" rel="stylesheet">
+
+
+    <title>CVTech</title>
+</head>
+
+<body>
+<div class="wrap">
+    <header class="header-home">
+
+        <h1>CVTech</h1>
+
+        <nav class="nav-home">
+        <ul>
+            <li><a class="navlogo" href="#"><img src="assets/img/logocvtech.png" alt="logo_nav"></a></li>
+            <li><a class="button btn-1" href="index.php">home</a></li>
+            <li><a class="button btn-1" href="">about</a></li>
+            <li><a class="button btn-1" href="">service</a></li>
+            <li><a class="button btn-1" id="login" href="login.php">connexion</a></li>
+            <li><a class="button btn-1" href="register.php">inscription</a></li>
+        </ul>
+        </nav>
+
+    </header>
+</div>
+<div class="clear"></div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include('inc/header.php'); ?>
+   <div class="wrap2">
+        <div class="service">
+                <div id="box-service" class="box-candidat">
+                    <img src="assets/img/candidat.jpg" alt="">
+
+                </div>
+                <div id="box-service" class="box-recruteur">
+                    <img src="assets/img/recruteur.jpg" alt="">
+
+                </div>
+        </div>
+    </div>
+
+<?php
+include('inc/footer.php');
