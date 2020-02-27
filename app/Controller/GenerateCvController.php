a3e99959e22acae996fab56e26a916802eb4ecb2
stephane <github@lecuyer.eu>
2020-02-27 13:48:04 +0100

    Set up of live mail generation:  - Adding the various inputs  - Set up on a fictitious CV  - Input error checking

--- app/Controller/GenerateCvController.php
+++ app/Controller/GenerateCvController.php
@@ -14,12 +14,19 @@ class GenerateCvController extends Controller
 {
     public function generateCV()
     {
-        $message = 'Coucou';
+        $message = 'Génération de votre CV';
         $errors = array();
         if (!empty($_POST['submitted'])) {
             $post = $this->cleanXss($_POST);
             $valid = new Validation();
-            $errors['test'] = $valid->textValid($post['test'], 'test', 2, 20);
+            $errors['titre'] = $valid->textValid($post['titre'], 'titre de CV', 2, 20);
+            $errors['nom'] = $valid->textValid($post['nom'], 'nom et prénom', 2, 20);
+            $errors['numAdress'] = $valid->textValid($post['numAdress'], 'numéro de rue', 1, 10);
+            $errors['street'] = $valid->textValid($post['street'], 'nom de la rue', 2, 50);
+            $errors['city'] = $valid->textValid($post['city'], 'nom de la ville', 2, 50);
+            $errors['phone'] = $valid->phoneNumber($post['phone'], 'numéro de téléphone');
+            $errors['mail'] = $valid->emailValid($post['mail']);
+            $errors['lien'] = $valid->urlValidate($post['lien']);
         }
         $form = new Form($errors);
         $this->render('app.testGenerateCv.testgenerateCv', array(
--- view/app/testGenerateCv/testgenerateCv.php
+++ view/app/testGenerateCv/testgenerateCv.php
@@ -2,13 +2,49 @@
 <h1 style="font-size:25px;margin-top: 100px;margin-left: 50px; color: darkolivegreen">
     <?= $message; ?>
 </h1>
-<form action="#" method="post">
-    <?php
-    echo $form->label('test');
-    echo $form->input('test', null, 'onKeyup(this)');
-    echo $form->error('test');
-    echo '<br><br>' . $form->submit();
-    ?>
-</form>
+<div id="form" style="float: left;margin-left: 10px;width: 40%;">
+    <form action="#" method="post">
+        <?php
+        echo $form->label('titre de votre CV : ');
+        echo $form->input('titre', null, 'onKeyup(this,\'titreCV\')');
+        echo $form->error('titre');
+        echo "<br>" . $form->label('nom et prénom : ');
+        echo $form->input('nom', null, 'onKeyup(this,\'nomSurname\')');
+        echo $form->error('nom');
+        echo "<br>" . $form->label('n° de rue : ');
+        echo $form->input('numAdress', null, 'onKeyup(this,\'num\')');
+        echo $form->error('numAdress');
+        echo "<br>" . $form->label('nom de rue : ');
+        echo $form->input('street', null, 'onKeyup(this,\'nameStreet\')');
+        echo $form->error('street');
+        echo "<br>" . $form->label('ville : ');
+        echo $form->input('city', null, 'onKeyup(this,\'nameCity\')');
+        echo $form->error('city');
+        echo "<br>" . $form->label('n° de téléphone : ');
+        echo $form->input('phone', null, 'onKeyup(this,\'phoneNumber\')');
+        echo $form->error('phone');
+        echo "<br>" . $form->label('adresse mail : ');
+        echo $form->input('mail', null, 'onKeyup(this,\'mailAdress\')');
+        echo $form->error('mail');
+        echo "<br>" . $form->label('lien(s) : ');
+        echo $form->input('lien', null, 'onKeyup(this,\'lienInternet\')');
+        echo $form->error('lien');
+        echo '<br><br>' . $form->submit();
+        ?>
+    </form>
+</div>
 
-<br><p id="cvTest"></p>
+<div id="cv" style="float: right;margin-right: 300px; width: 350px; height: 500px; background-color: aquamarine;">
+    <div id="titleCV" style="margin-top: 8px; margin: 0 auto;background-color: tomato; text-align: center;"><p id="titreCV" style="width: 100%;word-break: break-all;"></p></div>
+    <div id="coord" style="background-color: darkgray; margin-top: 8px; width: 20%;margin-left: 10px">
+        <p id="nomSurname" style="width: 100%;word-break: break-all;"></p>
+        <div id="adresse" style="background-color: darkcyan">
+            <p id="num" style="width: 100%;word-break: break-all;"></p>
+            <p id="nameStreet" style="width: 100%;word-break: break-all;"></p>
+            <p id="nameCity" style="width: 100%;word-break: break-all;"></p>
+        </div>
+        <p id="phoneNumber" style="width: 100%;word-break: break-all;"></p>
+        <p id="mailAdress" style="width: 100%;word-break: break-all;"></p>
+        <p id="lienInternet" style="width: 100%;word-break: break-all;"></p>
+    </div>
+</div>
