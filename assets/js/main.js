ac3c4ad98e0e627065b41ca7c9316ab40fbb905e
Le-Duude <57392459+Le-Duude@users.noreply.github.com>
2020-02-27 15:21:14 +0100

    services section -> scrollreveal : ok new wrap2 in a wrap3 changing color on scrollbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-ScrollReveal().reveal('.service_candidat0, .service_candidat05, .service_candidat1,.service_candidat2,.service_candidat3,.service_recruteur1,.service_recruteur2,.service_recruteur3', {
+ScrollReveal().reveal('.service_candidat0, .service_candidat1', {
     delay: 500,
-    interval:300,
+    interval:500,
     reset:true
 });
-ScrollReveal().reveal('.service_candidat1,.service_candidat2,.service_candidat3,.service_recruteur1,.service_recruteur2,.service_recruteur3', {
-    delay: 500,
-    interval:300,
+ScrollReveal().reveal('.service_candidat2,.service_candidat3,.service_candidat4,.service_candidat5', {
+    delay: 1500,
+    interval:500,
     reset:true
 });
-ScrollReveal().reveal('.service_recruteur1,.service_recruteur2,.service_recruteur3', {
-    delay: 500,
-    interval:300,
+ScrollReveal().reveal('.service_recruteur0, .service_recruteur1, .service_recruteur2, .service_recruteur3', {
+    delay: 1000,
+    interval:500,
     reset:true
 });
--- index.php
+++ index.php
@@ -1,38 +1,42 @@
 <?php
 include('inc/header.php'); ?>
-
     <div class="wrap2">
-        <section class="services">
-            <div class="service box_candidat">
-                <div class="imgs_services"><img src="assets/img/candidat640.jpg"
-                                                srcset="assets/img/candidat320.jpg 320w,
+        <div class="wrap3">
+            <section class="services">
+                <div class="service box_candidat">
+                    <div class="imgs_services"><img src="assets/img/candidat640.jpg"
+                                                    srcset="assets/img/candidat320.jpg 320w,
                                                         assets/img/candidat640.jpg 640w"
-                                                sizes="(max-width: 320px) 280px,
+                                                    sizes="(max-width: 320px) 280px,
                                                        (max-width: 640px) 440px,
                                                         640px"
-                                                alt=""></div>
-                <div class="text_candidat">
-                    <h2 id="titreservices" class="service_candidat0">nos services candidats <br> <span class="service_candidat05">en toute simplicité</span></h2>
-                    <p id="servicesliste" class="service_candidat1">Inscrivez-vous</p>
-                    <p id="servicesliste" class="service_candidat2">Créer votre CV <span id="servicesliste" class="service_candidat3">ou déposez en un déjà existant.</span></p>
-                    <p id="servicesliste" class="service_candidat4">Patientez</p>
+                                                    alt=""></div>
+                    <div class="text_candidat">
+                        <h2 id="titreservices1" class="service_candidat0">nos services candidats</h2>
+                        <p class="service_candidat1">... en toute simplicité</p>
+                        <p id="servicesliste" class="service_candidat2">Inscrivez-vous</p>
+                        <p id="servicesliste" class="service_candidat3">Créer votre CV ... <span
+                                class="service_candidat4">ou déposez en un déjà existant.</span></p>
+                        <p id="servicesliste" class="service_candidat5">Patientez</p>
+                    </div>
                 </div>
-            </div>
-            <div class="service box_recruteur">
-                <div class="imgs_services"><img src="assets/img/recruteur640.jpg"
-                                                srcset="assets/img/recruteur320.jpg 320w,
+                <div class="service box_recruteur">
+                    <div class="imgs_services"><img src="assets/img/recruteur640.jpg"
+                                                    srcset="assets/img/recruteur320.jpg 320w,
                                                         assets/img/recruteur640.jpg 640w"
-                                                sizes="(max-width: 320px) 280px,
+                                                    sizes="(max-width: 320px) 280px,
                                                        (max-width: 640px) 440px,
                                                         640px""
-                                                alt=""></div>
-                <div class="text_recruteur">
-                    <h2 id="titreservices">nos services recruteurs</h2>
-                    <p class="service_recruteur1">Inscrivez-vous</p>
-                    <p class="service_recruteur2">Triez les candidats selon vos besoins</p>
-                    <p class="service_recruteur3"></p>
-                </div>
-        </section>
+                        alt="">
+                    </div>
+                    <div class="text_recruteur">
+                        <h2 id="titreservices2" class="service_recruteur0">nos services recruteurs</h2>
+                        <p id="servicesliste" class="service_recruteur1">Inscrivez-vous</p>
+                        <p id="servicesliste" class="service_recruteur2">Triez les candidats selon vos besoins</p>
+                        <p id="servicesliste" class="service_recruteur3">Faites votre choix !</p>
+                    </div>
+            </section>
+        </div>
     </div>
 
 
