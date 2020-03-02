baf495468ba3ae72b3b94a029d722f50f70cf385
stephane <github@lecuyer.eu>
2020-03-02 11:33:07 +0100

    CV Generation - Add CDN for Slim Select

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CVTech</title>
     <link rel="stylesheet" type="text/css" href="asset/css/style.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.25.0/slimselect.min.js"></script>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.25.0/slimselect.min.css" rel="stylesheet">
   </head>
   <body>
 
